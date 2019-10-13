a02c0605d0dab33b1abdd7b9c41a2b27c5435a56
zafar <zfrahmad@gmail.com>
2019-10-13 10:37:01 -0700

    hello

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String Name = "Test123";
+            Console.WriteLine(Name);
+            Console.ReadLine();
+        }
+    }
+}
